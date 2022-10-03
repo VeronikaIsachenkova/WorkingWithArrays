169193f78b39c14e9774d7a0539b04b8bdb378f5
Veronika <v.shishkowa@yandex.ru>
2022-10-03 21:56:22 +0300

    Добавила решение

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] value = { "15", "hello", "600", "4", "control Work", "by", "Isachenkova" };
+void Examination(string[] values)
+{
+    int count = 0;
+    int j = 0;
+    for (int i = 0; i < values.Length; i++)
+    {
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            count++;
+        }
+
+    }
+    string[] valuesTwo = new string[count];
+    
+    for (int i = 0; i < values.Length; i++)
+    {
+        
+        string number = values[i];
+        if (number.Length <= 3)
+        {
+            valuesTwo[j] = number;
+            Console.Write($"{valuesTwo[j]} ");
+            j++;
+        }
+
+    }
+
+}
+Examination(value);
